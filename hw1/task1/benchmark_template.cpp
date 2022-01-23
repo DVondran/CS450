dc301c422b2b3c997983890ece97a6bfa360ee87
DVondran <dvondran@hotmail.com>
2022-01-23 15:24:35 -0500

    u

--- hw1/task1/benchmark_template.cpp
+++ hw1/task1/benchmark_template.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 	double *C = new double[N];
 	double *D = new double[N];
 	
-	for(i = 0; i < N, i++){
+	for(int i = 0; i < N, i++){
 		A[i] = 0.0;
 		B[i] = 1.0;
 		C[i] = 2.0;
@@ -59,8 +59,8 @@ int main(int argc, char *argv[])
 	get_walltime(&d_S);  // get start time
 	// place test code here
 	
-	for(i = 0; i < R; i++){
-		for(j=0; j < N; j++){
+	for(int i = 0; i < R; i++){
+		for(int j=0; j < N; j++){
 			A[i] = B[i] + C[i] * D[i];
 		}
 		if(A[2] < 0){
