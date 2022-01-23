977b2916f29292a814f12be2803c204d8bd7ae14
DVondran <dvondran@hotmail.com>
2022-01-23 14:57:09 -0500

    commit 2

--- hw1/task 1/benchmark_template.cpp
+++ hw1/task 1/benchmark_template.cpp
@@ -28,6 +28,10 @@ void get_walltime(double* wcTime) {
 
 }
 
+void dummy(double* one, double* two, double* three, double* four){
+	;
+}
+
 ///////////////////////////////////////////////////////////////////////
 //
 //
@@ -36,18 +40,41 @@ int main(int argc, char *argv[])
 {
 
 	// Initialize stuff here...
+	int N = 1000;
+	int R = 1000000;
 	double d_S,d_E;
+	double *A = new double[N];
+	double *B = new double[N];
+	double *C = new double[N];
+	double *D = new double[N];
+	
+	for(i = 0; i < N, i++){
+		A[i] = 0.0;
+		B[i] = 1.0;
+		C[i] = 2.0;
+		D[i] = 3.0;
+	}
 
 
 	get_walltime(&d_S);  // get start time
 	// place test code here
+	
+	for(i = 0; i < R; i++){
+		for(j=0; j < N; j++){
+			A[i] = B[i] + C[i] * D[i];
+		}
+		if(A[2] < 0){
+			dummy(A, B, C, D);
+		}
+	}
 
 
 	get_walltime(&d_E); // get end time stamp
 
 	// report results here
+	int MFLOPS = (double)R*(double)N*2/((d_E - d_S)*1.0e6)
 	printf("Elapsed time: %f\n", d_E - d_S);
-	
+	printf("MFLOPS: %f\n", MFLOPS);
 	
 	// perform cleanup here
 	
