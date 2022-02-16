1716cfe98f544d47edf35a0f694af7360eaf0c37
DVondran <dvondran@hotmail.com>
2022-02-15 20:20:28 -0500

    q

--- 2022_Spring_Hw02_Part1/abc123_hw2_part1/hw2_part1.cpp
+++ 2022_Spring_Hw02_Part1/abc123_hw2_part1/hw2_part1.cpp
@@ -16,10 +16,10 @@ void myfunc(double *s, double *mat, int *v, int length)
 		int jlen = j * length;
 		for (int i = 0; i < length; i++)
 		{
-			val = round(fmod(v[i],256)); 
-			double sinval = sin(val)*sin(val);
-			double squaredval = sinval - (1.0 - sinval);
-			mat[i + jlen] = s[i + jlen]*(squaredval);
+			val = round(fmod(v[i],256));
+			//double sinval = sin(val)*sin(val);
+			//double squaredval = sin(val)*sin(val) - (1.0 - sin(val)*sin(val));
+			mat[i + jlen] = s[i + jlen]*(sin(val)*sin(val) - (1.0 - sin(val)*sin(val)));
 		}
 	}
 }
