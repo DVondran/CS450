ae2cff5d404fdf2617fee812fcafd90c82d4584c
DVondran <dvondran@hotmail.com>
2022-02-16 19:09:00 -0500

    q

--- 2022_Spring_Hw02_Part1/abc123_hw2_part1/hw2_part1.cpp
+++ 2022_Spring_Hw02_Part1/abc123_hw2_part1/hw2_part1.cpp
@@ -12,6 +12,7 @@ PSU ID: drv5114
 
 //Testing Working
 
+//Mathematics inside the algorithm removed for simplification
 double getSquaredVal(double val)
 {
 	double sinval = sin(val)*sin(val);
@@ -31,16 +32,12 @@ void myfunc(double *s, double *mat, int *v, int length)
 		val[i] = round(fmod(v[i],256));
 	}
 	
-	for (int j = 0; j < length; j += 5)
+	for (int j = 0; j < length; j ++)
 	{
 		int jlen = j * length;
 		for (int i = 0; i < length; i ++)
 		{
 			mat[i + jlen] = s[i + jlen]*(getSquaredVal(val[i]));
-			mat[i + jlen + 2*length] = s[i + jlen + 2*length]*(getSquaredVal(val[i]));
-			mat[i + jlen + 3*length] = s[i + jlen + 3*length]*(getSquaredVal(val[i]));
-			mat[i + jlen + 4*length] = s[i + jlen + 4*length]*(getSquaredVal(val[i]));
-			mat[i + jlen + 5*length] = s[i + jlen + 5*length]*(getSquaredVal(val[i]));
 		}
 	}
 }
