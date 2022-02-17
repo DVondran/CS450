c898e9d021d8e8b0f3899e9db32df80f07fa9cd1
DVondran <dvondran@hotmail.com>
2022-02-16 20:31:28 -0500

    q

--- 2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
+++ 2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
@@ -3,12 +3,13 @@
 void matrix_mult(double *A, double *B, double *C, int N)
 {
 	for (int i = 0; i < N; i++)
+		inval = i * N;
 		for (int j = 0; j < N; j++)
 		{
 			C[i * N + j] = 0;
 				for (int k = 0; k < N; k++)
 				{
-					C[i * N + j] += A[i * N + k] * B[k * N + j];
+					C[inval + j] += A[inval + k] * B[k * N + j];
 				}
 		}
 }
