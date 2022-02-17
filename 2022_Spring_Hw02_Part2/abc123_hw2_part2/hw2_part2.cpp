36afb00a660a74d84c735bdb4b20c2fb841896bf
DVondran <dvondran@hotmail.com>
2022-02-16 22:21:04 -0500

    q

--- 2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
+++ 2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
@@ -25,15 +25,15 @@ void matrix_mult(double *A, double *B, double *C, int N)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			C[i * N + j] = 0;
-				for (int k = 0; k < N; k++)
-				{
-					C[i * N + j] += A[i * N + k] * B[k * N + j];
-					C[(i + 1) * N + j] += A[(i + 1) * N + k] * B[k * N + j];
-					C[(i + 2) * N + j] += A[(i + 2) * N + k] * B[k * N + j];
-					C[(i + 3) * N + j] += A[(i + 3) * N + k] * B[k * N + j];
-					C[(i + 4) * N + j] += A[(i + 4) * N + k] * B[k * N + j];
-				}
+			//C[i * N + j] = 0;
+			for (int k = 0; k < N; k++)
+			{
+				C[i * N + j] += A[i * N + k] * B[k * N + j];
+				C[(i + 1) * N + j] += A[(i + 1) * N + k] * B[k * N + j];
+				C[(i + 2) * N + j] += A[(i + 2) * N + k] * B[k * N + j];
+				C[(i + 3) * N + j] += A[(i + 3) * N + k] * B[k * N + j];
+				C[(i + 4) * N + j] += A[(i + 4) * N + k] * B[k * N + j];
+			}
 		}
 	}
 }
