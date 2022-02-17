c8851af3dd5a58fdc583a0cfaeb9d9d4ecec23ea
DVondran <dvondran@hotmail.com>
2022-02-16 21:53:35 -0500

    q

--- 2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
+++ 2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
@@ -1,16 +1,39 @@
 
+/*
+
+AUTHOR: David Vondran
+PSU ID: drv5114
+
+*/
+
 // complex algorithm for evaluation
 void matrix_mult(double *A, double *B, double *C, int N)
 {
-	for (int i = 0; i < N; i++)
-		inval = i * N;
+	/*
+	double *Anew = double[N*N]
+	//getting Anew mapped in n^2 time
+	for (int i = 0; i < N; i ++)
+	{
+		int inew = i * N;
+		for (int j = 0; j < N; j++)
+		{
+			Anew[i] = A[inew + k];
+		}
+	}
+	*/
+	for (int i = 0; i < N; i+= 5)
+	{
 		for (int j = 0; j < N; j++)
 		{
 			C[i * N + j] = 0;
 				for (int k = 0; k < N; k++)
 				{
-					C[inval + j] += A[inval + k] * B[k * N + j];
+					C[i * N + j] += A[i * N + k] * B[k * N + j];
+					C[(i + 1) * N + j] += A[(i + 1) * N + k] * B[k * N + j];
+					C[(i + 2) * N + j] += A[(i + 2) * N + k] * B[k * N + j];
+					C[(i + 3) * N + j] += A[(i + 3) * N + k] * B[k * N + j];
+					C[(i + 4) * N + j] += A[(i + 4) * N + k] * B[k * N + j];
 				}
 		}
-}
+	}
 
