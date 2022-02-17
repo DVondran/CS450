db69169aa6d189fd5551cde15f59035cf871e393
DVondran <dvondran@hotmail.com>
2022-02-16 19:16:50 -0500

    q

--- 2022_Spring_Hw02_Part1/abc123_hw2_part1/hw2_part1.cpp
+++ 2022_Spring_Hw02_Part1/abc123_hw2_part1/hw2_part1.cpp
@@ -34,12 +34,20 @@ void myfunc(double *s, double *mat, int *v, int length)
 		squaredvals[i] = getSquaredVal(val[i]);
 	}
 	
-	for (int j = 0; j < length; j ++)
+	for (int j = 0; j < length; j += 5)
 	{
 		int jlen = j * length;
-		for (int i = 0; i < length; i ++)
+		int jlen1 = (j+1) * length;
+		int jlen2 = (j+2) * length;
+		int jlen3 = (j+3) * length;
+		int jlen4 = (j+4) * length;
+		for (int i = 0; i < length; i +=5)
 		{
 			mat[i + jlen] = s[i + jlen]*(squaredvals[i]);
+			mat[i + 1 + jlen1] = s[i + 1 + jlen1]*(squaredvals[i + 1]);
+			mat[i + 2 + jlen2] = s[i + 2 + jlen2]*(squaredvals[i + 2]);
+			mat[i + 3 + jlen3] = s[i + 3 + jlen3]*(squaredvals[i + 3]);
+			mat[i + 4 + jlen4] = s[i + 4 + jlen4]*(squaredvals[i + 4]);
 		}
 	}
 }
