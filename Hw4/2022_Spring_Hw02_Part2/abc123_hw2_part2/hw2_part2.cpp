67fc69736c10d35ac3a8de658bd6ae17a638bc92
DVondran <dvondran@hotmail.com>
2022-03-27 18:53:42 -0400

    q

--- Hw4/2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
+++ Hw4/2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
@@ -8,14 +8,16 @@
 // complex algorithm for evaluation
 void matrix_mult(double *A, double *B, double *C, int N)
 {
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-		{
-			C[i * N + j] = 0;
-				for (int k = 0; k < N; k++)
-				{
-					C[i * N + j] += A[i * N + k] * B[k * N + j];
-				}
+	#pragma omp parallel num_threads(P){
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+			{
+				C[i * N + j] = 0;
+					for (int k = 0; k < N; k++)
+					{
+						C[i * N + j] += A[i * N + k] * B[k * N + j];
+					}
 		}
+	}
 }
 
