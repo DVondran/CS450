3f4925a4af1f914e69136f6fc11f93d88900eef7
DVondran <dvondran@hotmail.com>
2022-03-27 19:17:52 -0400

    q

--- Hw4/2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
+++ Hw4/2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.cpp
@@ -12,9 +12,11 @@ void matrix_mult(double *A, double *B, double *C, int N)
 	{
 		#pragma omp for
 		for (int i = 0; i < N; i++)
+			#pragma omp for
 			for (int j = 0; j < N; j++)
 			{
 				C[i * N + j] = 0;
+					#pragma omp for
 					for (int k = 0; k < N; k++)
 					{
 						C[i * N + j] += A[i * N + k] * B[k * N + j];
