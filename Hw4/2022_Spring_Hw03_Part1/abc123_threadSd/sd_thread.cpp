702f4ad87bb48d3f5ef503e55852723e20aa7a10
DVondran <dvondran@hotmail.com>
2022-03-27 20:01:40 -0400

    q

--- Hw4/2022_Spring_Hw03_Part1/abc123_threadSd/sd_thread.cpp
+++ Hw4/2022_Spring_Hw03_Part1/abc123_threadSd/sd_thread.cpp
@@ -86,14 +86,10 @@ THRESH_RESULT *findThreshValuesThread(double *A, long N, double T, int P)
 	c = 0;
 	
 	// store the index locations of the values over threshold
-	#pragma omp parallel num_threads(P)
-	{
-		#pragma omp for private(i) reduction(+:c)
-		for (long i=0; i < N; i++){
-			if (A[i] > T){
-				p_tmpResult->pli_list[c] = i;
-				c++;
-			}
+	for (long i=0; i < N; i++){
+		if (A[i] > T){
+			p_tmpResult->pli_list[c] = i;
+			c++;
 		}
 	}
 	
