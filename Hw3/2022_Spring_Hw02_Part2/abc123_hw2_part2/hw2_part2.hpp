b767787ced48095f3146e0ee9d54867ebe39b643
DVondran <dvondran@hotmail.com>
2022-04-29 19:27:40 -0400

    q

--- Hw3/2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.hpp
+++ Hw3/2022_Spring_Hw02_Part2/abc123_hw2_part2/hw2_part2.hpp
@@ -1,8 +1,6 @@
 /**
  * This is a simple header file that declares the function in the library.
  */
- 
-#ifndef hw2_part2
-#define hw2_part2
+
 
 void matrix_mult(double *A, double *B, double *C, int N, int P);
